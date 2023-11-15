415424cc7e47ffb01f890bd817b428826c1aa218
Rickaym <psmtitant@gmail.com>
2023-11-15 17:10:46 +0630

    remove: yangon bounds

--- src/routes/buslines/[busLine]/+page.svelte
+++ src/routes/buslines/[busLine]/+page.svelte
@@ -5,10 +5,6 @@
     import L from "leaflet";
     export let data: PageData;
 
-    var corner1 = L.latLng(17.090266, 95.967204),
-        corner2 = L.latLng(16.587593, 96.368538),
-        ygnBounds = L.latLngBounds(corner1, corner2);
-
     const routeArray = [
         L.latLng(
             data.routedBusStops[0].stop.latitude,
@@ -33,12 +29,8 @@
                 <div>{routedStop.stop.stopName}</div>
             {/each}
         </div>
-        <div class="w-full h-screen">
-            <Leaflet
-                bounds={ygnBounds}
-                latLngRouteArray={routeArray}
-                zoom={14}
-            />
-        </div>
+        <Leaflet
+            latLngRouteArray={routeArray}
+        />
     </div>
 </div>
