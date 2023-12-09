6e1cb293dd3bd340f2bbf847d3c2440c76f488cc
Rickaym <psmtitant@gmail.com>
2023-12-09 18:01:22 +0630

    feat: display leaflet map as background everywhere

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
   import "../app.postcss";
+
+  import { onMount } from "svelte";
+
+  let Leaflet: any;
+
+  onMount(async () => {
+    // dynamically import the CSR component and lib
+    Leaflet = (await import("$lib/components/Leaflet.svelte")).default;
+  });
 </script>
+
+{#if Leaflet}
+  <svelte:component this={Leaflet} />
+{/if}
 
 <slot />
