f0e0a20e2a13d19547eadd000ce5c7306e268bc4
Rickaym <psmtitant@gmail.com>
2023-12-09 18:00:25 +0630

    feat: add back button component

---
+++ src/lib/components/BackButton.svelte
@@ -0,0 +1,3 @@
+<a href="../" class="bg-white border w-20 h-10 rounded-3xl m-8 custom-box-shadow" style="border-color: var(--color-primary);">
+    <img class="w-8 ms-3 mt-0.5" src="/arrow-left.svg" alt="arrow-left">
+</a>
