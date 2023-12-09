da4038b911046dd5352d640765d9944ff4d51814
Rickaym <psmtitant@gmail.com>
2023-12-09 18:00:32 +0630

    feat: add bottom slide component

---
+++ src/lib/components/BottomSlide.svelte
@@ -0,0 +1,8 @@
+<div
+    class="pt-2"
+    id="results"
+    style="height: 80vh; width: 100vw; background-color: #14262366; backdrop-filter: blur(7.800000190734863px);"
+>
+    <img class="w-8 mx-auto" src="round-drag-handle.svg" alt="round-drag-handle">
+    <slot />
+</div>
