b31839eb96d547d173b55afa665bbc91afbd001c
Rickaym <psmtitant@gmail.com>
2023-12-29 17:24:30 +0630

    fix: add typing and resolve linting errors

--- src/lib/stores/search.ts
+++ src/lib/stores/search.ts
@@ -1,12 +1,18 @@
 import { writable } from "svelte/store";
-import type { BusStop } from '@prisma/client';
+import type { BusStop } from "$lib/db";
 
+
+type SearchStore = {
+    data: BusStop[];
+    filtered: BusStop[];
+    search: string;
+};
 
 export function createSearchStore(data: BusStop[]) {
     const { subscribe, set, update } = writable({
         data,
         filtered: data,
-        search: '',
+        search: ''
     });
 
     return {
@@ -16,9 +22,9 @@ export function createSearchStore(data: BusStop[]) {
     };
 }
 
-export function searchHandler(store: any) {
-    const searchTerm = store.search.toLowerCase() || "";
-    store.filtered = store.data.filter((item: any) => {
-        return item.stopName.toLowerCase().includes(searchTerm);
+export function searchHandler(store: SearchStore) {
+    const searchTerm = store.search.toLowerCase();
+    store.filtered = store.data.filter((item) => {
+        return item.name.toLowerCase().includes(searchTerm);
     });
 }
