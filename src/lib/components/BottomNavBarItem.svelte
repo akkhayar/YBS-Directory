361b3b2b0b61400b87a41100e3f7714a624ef755
Rickaym <psmtitant@gmail.com>
2024-01-01 15:04:45 +0630

    feat: better designs

--- src/lib/components/BottomNavBarItem.svelte
+++ src/lib/components/BottomNavBarItem.svelte
@@ -6,15 +6,13 @@
 
 <a href={url}>
     <button
-        class="flex-none bg-white w-16 h-10 rounded-3xl border border-solid custom-box-shadow sm:flex sm:justify-evenly sm:w-20 sm:h-8"
+        class="flex justify-center items-center bg-white w-16 h-10 rounded-3xl border border-solid custom-box-shadow sm:w-fit sm:h-8 sm:px-3 sm:py-2"
         style="border-color: var(--color-primary);"
     >
-        <img class="w-5 m-auto sm:mx-0 sm:w-4" src={icon} alt="search-icon" />
-        <p
-            class="me-1 text-center text-xs leading-normal hidden my-auto sm:block"
-        >
+        <img class="w-5 sm:mx-0 sm:w-4" src={icon} alt="search-icon" />
+        <span class="ms-1 text-xs hidden sm:block" style="line-height: 3;">
             {label}
-        </p>
+        </span>
     </button>
     <p class="text-center text-xs mt-1 leading-normal sm:hidden">{label}</p>
 </a>
