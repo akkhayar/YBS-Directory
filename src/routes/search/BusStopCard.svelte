3d2854a082acf7bee600314f13ec22f308a624bc
Rickaym <psmtitant@gmail.com>
2024-01-03 09:34:00 +0630

    feat: add location svg

--- src/routes/search/BusStopCard.svelte
+++ src/routes/search/BusStopCard.svelte
@@ -6,7 +6,10 @@
 </script>
 
 <button on:click={onClick} class="text-left my-2">
-    <div class="py-1 my-lang">{busStop.name}</div>
+    <div class="flex">
+        <img src="/location.svg" alt="location" class="w-3" />
+        <div class="py-1 ps-2 my-lang">{busStop.name}</div>
+    </div>
     <div class="font-light text-xs my-lang">{busStop.address}</div>
 </button>
 <br />
