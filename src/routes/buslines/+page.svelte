2b9846ca4a05584d27afafeead685b2df724d2cf
Rickaym <psmtitant@gmail.com>
2024-01-03 09:33:49 +0630

    feat: rewrite using bottom slide components

--- src/routes/buslines/+page.svelte
+++ src/routes/buslines/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import BottomSlide from "$lib/components/BottomSlide.svelte";
+    import BottomSlideScrollable from "$lib/components/BottomSlideScrollable.svelte";
     import BackButton from "$lib/components/BackButton.svelte";
     import BusCard from "./BusCard.svelte";
     import type { PageData } from "./$types";
@@ -7,11 +8,27 @@
     export let data: PageData;
 </script>
 
-<div class="flex flex-col justify-between h-screen">
+<div class="flex h-screen flex-col justify-between">
     <BackButton />
     <BottomSlide>
-        {#each data.busLines as bus}
-            <BusCard busLine={bus} />
-        {/each}
+        <input
+            class="mb-3 mt-5 flex h-10 flex-grow justify-between rounded-full border border-solid bg-white px-5 shadow sm:w-80 sm:flex-none"
+            style="border-color: var(--color-primary);"
+            type="search"
+        />
+        <BottomSlideScrollable>
+            {#each data.busLines as bus}
+                <!-- <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} />
+                <BusCard busLine={bus} /> -->
+                <BusCard busLine={bus} />
+            {/each}
+        </BottomSlideScrollable>
     </BottomSlide>
 </div>
