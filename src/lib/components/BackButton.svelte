36891f41a457a3844cedf707cd637734389cd904
Rickaym <psmtitant@gmail.com>
2024-01-04 11:50:25 +0630

    feat: use absolute styling for back button

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,6 +1,6 @@
 <a
     href="../"
-    class="bg-white border w-20 h-10 rounded-3xl mx-6 my-5 custom-box-shadow"
+    class="absolute top-5 left-5 bg-white border w-20 h-10 rounded-3xl custom-box-shadow"
     style="border-color: var(--color-primary);"
 >
     <img class="w-8 ms-3 mt-0.5" src="/arrow-left.svg" alt="arrow-left" />
