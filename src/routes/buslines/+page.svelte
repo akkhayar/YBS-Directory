c95c5df58ba0c8835f099f8554d97e2585084ae6
Rickaym <psmtitant@gmail.com>
2024-01-04 11:51:53 +0630

    feat: finalize busline page

--- src/routes/buslines/+page.svelte
+++ src/routes/buslines/+page.svelte
@@ -8,27 +8,25 @@
     export let data: PageData;
 </script>
 
-<div class="flex h-screen flex-col justify-between">
-    <BackButton />
-    <BottomSlide>
-        <input
-            class="mb-3 mt-5 flex h-10 flex-grow justify-between rounded-full border border-solid bg-white px-5 shadow sm:w-80 sm:flex-none"
-            style="border-color: var(--color-primary);"
-            type="search"
-        />
-        <BottomSlideScrollable>
-            {#each data.busLines as bus}
-                <!-- <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} />
-                <BusCard busLine={bus} /> -->
-                <BusCard busLine={bus} />
-            {/each}
-        </BottomSlideScrollable>
-    </BottomSlide>
-</div>
+<BackButton />
+<BottomSlide>
+    <input
+        class="mb-3 mt-5 flex h-10 flex-grow justify-between rounded-full border border-solid bg-white px-5 shadow sm:w-80 sm:flex-none"
+        style="border-color: var(--color-primary);"
+        type="search"
+    />
+    <BottomSlideScrollable>
+        {#each data.busLines as bus}
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+            <BusCard busLine={bus} />
+        {/each}
+    </BottomSlideScrollable>
+</BottomSlide>
