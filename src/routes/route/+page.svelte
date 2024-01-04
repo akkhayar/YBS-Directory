6fdd55b3e7c22296d9eae9013b14489bb86c73f9
Rickaym <psmtitant@gmail.com>
2024-01-04 13:46:02 +0630

    feat: add route page

---
+++ src/routes/route/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import BackButton from "$lib/components/BackButton.svelte";
+</script>
+
+<BackButton />
+
+<div class="bg-glass absolute bottom-8 left-5 right-5 text-white p-5 rounded-3xl">
+    <p>Malikha Housing - Kudos Bakery</p>
+    <p class="te text-3xl">21 - 145</p>
+    <p class="text-lg">Time Estimate: approx. 30 mins</p>
+    <div class="flex">
+        <p class="text-xs font-light">
+            To bus stop: 3mins <br>
+            On bus: 24 mins <br>
+            To destination: 3mins
+        </p>
+        <button class="bg-white w-10 h-5 text-black rounded-lg">Select</button>
+    </div>
+</div>
