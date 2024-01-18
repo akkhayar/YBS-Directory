cd287caf4db5fa2d0ec2776b398248e830f9fbc9
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-01-18 14:33:03 +0630

    feat: add bus colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,35 @@ const config = {
     content: ["./src/**/*.{html,js,svelte,ts}"],
 
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    100: "#ffd231",
+                },
+                light: {
+                    900: "#ffe895",
+                },
+                busBlue: {
+                    100: "#3c2aa9",
+                },
+                busRed: {
+                    100: "#e4002b",
+                },
+                busPurple: {
+                    100: "#7a1fa2",
+                },
+                busCyan: {
+                    100: "#00a1e4",
+                },
+                busBrown: {
+                    100: "#a6703b",
+                },
+            },
+            textColor: {
+                dark: "#343434",
+                light: "#F4F4F4",
+            },
+        },
     },
 
     plugins: [],
