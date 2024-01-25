a6bf9a67a09d7b1bb0b44b3bb1063cc2d71ad596
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-01-26 00:13:06 +0630

    feat: add transitions

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,21 +1,35 @@
+<script>
+    import { send, receive } from "$lib/pageCrossFade";
+</script>
+
 <div
     aria-label="Navigation Bar"
     class="absolute top-0 flex w-full px-6 py-5 pb-1 sm:w-fit"
 >
     <a
+        in:receive={{ key: "block" }}
+        out:send={{ key: "block" }}
         aria-label="More Options"
         href="/settings"
-        class="me-6 flex h-10 w-10 rounded-3xl border border-solid border-primary-100 bg-white shadow"
+        class="custom-shadow me-6 flex h-10 w-10 rounded-3xl border border-solid border-primary-100 bg-white"
     >
-        <img class="mx-auto w-5" src="/assets/images/hamburger.svg" alt="search-icon" />
+        <img
+            class="mx-auto w-5"
+            src="/assets/images/hamburger.svg"
+            alt="search-icon"
+        />
     </a>
 
     <a
+        in:send={{ key: "search" }}
+        out:receive={{ key: "search" }}
         aria-label="Search Bar"
-        class="flex h-10 flex-grow justify-between rounded-full border border-solid border-primary-100 bg-white px-5 shadow sm:w-80 sm:flex-none text-gray-400 align-middle"
+        class="custom-shadow flex h-10 flex-grow justify-between rounded-full border border-solid border-primary-100 bg-white px-5 align-middle text-gray-400 sm:w-80 sm:flex-none"
         href="/search"
     >
-        <span class="Poppins font-light" style="line-height: 2.4;">Search bus stop..</span>
+        <span class="Poppins font-light" style="line-height: 2.4;"
+            >Search bus stop..</span
+        >
         <img
             class="filter-primary-100 ms-auto w-5"
             src="/assets/images/search-icon.svg"
@@ -23,9 +37,3 @@
         />
     </a>
 </div>
-
-<style>
-    .shadow {
-        filter: drop-shadow(0px 4px 10px rgba(0, 0, 0, 0.25));
-    }
-</style>
