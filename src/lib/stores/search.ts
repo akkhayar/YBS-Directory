0823e277525660235d215f2d2124311a23d88dba
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-01-26 02:52:05 +0630

    fix: take in search key

--- src/lib/stores/search.ts
+++ src/lib/stores/search.ts
@@ -1,7 +1,18 @@
 import { writable } from "svelte/store";
 import fuzzysort from "fuzzysort";
 
-export function createSearchStore<T>(data: T[], index: (val: T) => string) {
+/**
+ * Creates a search store with a complex object array. 
+ * 
+ * Returns an object with the data passed in, the filtered list and the
+ * search writable.
+ * 
+ * @param data 
+ * @param index 
+ * @param searchKey 
+ * @returns 
+ */
+export function createSearchStore<T>(data: T[], index: (val: T) => string, searchKey: string) {
     // filter bus stop objects based on name
     const uniqueData = Object.values(
         data.reduce(
@@ -23,11 +34,11 @@ export function createSearchStore<T>(data: T[], index: (val: T) => string) {
 
     search.subscribe((searchString) => {
         store.update((model) => {
-            if (searchString.length === 0) {
+            if (searchString.trimEnd().length === 0) {
                 model.filtered = model.data;
             } else {
                 model.filtered = fuzzysort
-                    .go(searchString, model.data, { key: "name_en" })
+                    .go(searchString, model.data, { key: searchKey })
                     .map((res) => res.obj);
             }
             return model;
