27fb58cd5a85f704a9ff62c950dd8ee37868c8bb
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-01-26 13:05:42 +0630

    fix: load only necessary data

--- src/routes/(bg-map)/buslines/+page.svelte
+++ src/routes/(bg-map)/buslines/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import type { PageData } from "./$types";
-    import type { BusLineGeoJSON } from "$lib/database.d";
     import { createSearchStore } from "$lib/stores/search";
     import { fade, slide } from "svelte/transition";
     import BottomDrawer from "$lib/components/ui/bottom-drawer/BottomDrawer.svelte";
@@ -8,25 +7,27 @@
     import HideSearchBar from "$lib/components/ui/HideSearchBar.svelte";
     import BackButton from "$lib/components/ui/BackButton.svelte";
     import BusCard from "./BusCard.svelte";
+    import type { GeoJsonProperties } from "geojson";
 
     export let data: PageData;
 
-    let searchStore = createSearchStore<BusLineGeoJSON>(
+    type ShallowBusLine = {
+        metadata: {
+            agency_id: string;
+            color: "red" | "blue" | "cyan" | "brown" | "purple";
+            route_id: string;
+            stops: string[];
+        };
+        properties: GeoJsonProperties;
+    };
+
+    let searchStore = createSearchStore<ShallowBusLine>(
         data.busLines,
         (busLine) => busLine.metadata.route_id,
         "metadata.route_id",
     );
 
     const search = $searchStore.search;
-
-    function getBusStopNameIndex(bus: BusLineGeoJSON, index: number) {
-        if (bus.metadata.stops.length === 0) return "";
-        return data.busStops[
-            bus.metadata.stops[
-                index === -1 ? bus.metadata.stops.length - 1 : index
-            ]
-        ].name_mm;
-    }
 </script>
 
 <BackButton />
@@ -34,11 +35,11 @@
 <BottomDrawer let:Scrollable let:Header>
     <Header>
         <div
-            in:slide={{ duration: 250, axis: "y", delay: 250 }}
+            in:slide={{ duration: 200, axis: "y", delay: 250 }}
             class="flex justify-start border-b-2 border-solid border-white bg-transparent px-3 pb-2"
         >
             <img
-                in:fade={{ duration: 250, delay: 200 }}
+                in:fade={{ duration: 200, delay: 200 }}
                 class="my-auto me-3 h-5 w-5"
                 src="/assets/images/search-icon.svg"
                 alt="search-icon"
@@ -58,10 +59,11 @@
             initialBuffer={6}
             let:data={bus}
         >
+            {@const name = bus.properties.name.split("-")}
             <BusCard
                 busLineId={bus.metadata.route_id}
-                busLineFirstStopName={getBusStopNameIndex(bus, 0)}
-                busLineLastStopName={getBusStopNameIndex(bus, -1)}
+                busLineFirstStopName={name[0]}
+                busLineLastStopName={name[1]}
                 totalStops={bus.metadata.stops.length}
                 cardColor={bus.metadata.color}
             />
