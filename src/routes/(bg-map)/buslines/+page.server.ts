cd9946b2f0745b75d05af96c2e91e7b072d25d3b
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-01-26 13:04:55 +0630

    fix: only load necessary details

--- src/routes/(bg-map)/buslines/+page.server.ts
+++ src/routes/(bg-map)/buslines/+page.server.ts
@@ -1,8 +1,10 @@
-import { getAllBusLines, getAllBusStops } from "$lib/server/database";
+import { getAllBusLines } from "$lib/server/database";
 
 export const load = () => {
     return {
-        busLines: Object.values(getAllBusLines()),
-        busStops: getAllBusStops(),
+        busLines: Object.values(getAllBusLines()).map((busLineGeoJSON) => ({
+            metadata: busLineGeoJSON.metadata,
+            properties: busLineGeoJSON.properties,
+        })),
     };
 };
